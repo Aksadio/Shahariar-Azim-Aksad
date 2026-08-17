a812105f3753b895fb95b8dc3d1a300bbdd7ccfd
Shahariar Azim Aksad <aksad.dev.io@gmail.com>
2026-08-17 10:53:25 +0600

    Update FeaturedVideo.jsx

--- components/Featured/FeaturedVideo.jsx
+++ components/Featured/FeaturedVideo.jsx
@@ -26,14 +26,14 @@ const FeaturedVideo = ({refForward, ...props }) => {
       variants={variants}
       initial="initial"
       animate={progress > 0.5 ? "animate" : "initial"}
-      className="relative md:absolute mx-auto md:mx-0 mt-4 md:mt-0 md:top-[55vh] md:left-20 md:translate-x-0 md:translate-y-0 z-30 w-[82vw] md:w-[40vw] max-w-[22rem] md:max-w-[856px] aspect-[3/4] md:aspect-[856/1024] overflow-hidden rounded-3xl"
+      className="relative md:absolute mx-auto md:mx-0 mt-4 md:mt-0 md:top-[55vh] md:left-20 md:translate-x-0 md:translate-y-0 z-30 w-[82vw] md:w-[40vw] max-w-[22rem] md:max-w-[856px] overflow-hidden rounded-3xl"
       {...props}
     >
      <img
-        src={`${BASE_PATH}/Portfolio_Img-4.png`}
-        alt="Featured portrait"
-        className="absolute inset-0 w-full h-full object-cover"
-      />
+  src={`${BASE_PATH}/Portfolio_Img-4.png`}
+  alt="Featured portrait"
+  className="block w-full h-auto object-contain"
+    />
     </motion.div>
   );
 };
