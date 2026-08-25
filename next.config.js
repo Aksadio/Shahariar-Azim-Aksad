ffb62b7fef1c72d4d3863ae16b427fa8f2d9699a
Shahariar Azim Aksad <aksad.dev.io@gmail.com>
2026-08-25 23:13:09 +0600

    Update repository name in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 // IMPORTANT: replace 'REPO_NAME' below with your actual GitHub repository name.
 // e.g. if your repo is github.com/yourname/my-portfolio, set REPO_NAME = 'my-portfolio'.
 // If you deploy to a USER/ORG page (repo named yourname.github.io), leave both empty strings ''.
-const REPO_NAME = 'my-portfolio';
+const REPO_NAME = 'Shahariar-Azim-Aksad';
 
 const nextConfig = {
   output: 'export',
